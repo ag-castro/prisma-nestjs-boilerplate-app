f6197d6828eeba2600f0cb53ac4de4fe7ea8f126
Andre Gustavo Castro <agcastro.py@yahoo.com>
2020-02-14 21:11:36 -0300

    Added authentication and posts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
-import { Module } from '@nestjs/common'
+import { Module, ValidationPipe } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
 import { GraphqlOptions } from './graphql.options'
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './auth/auth.module'
+import { APP_PIPE } from '@nestjs/core'
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
@@ -9,8 +12,15 @@ import { PrismaModule } from './prisma/prisma.module';
       useClass: GraphqlOptions,
     }),
     PrismaModule,
+    AuthModule,
+    PostModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { Strategy } from 'passport-jwt'
+import { Request } from 'express'
+import { AuthService } from './auth.service'
+import { User } from '../../generated/prisma-client'
+
+const cookieExtractor = (req: Request): string | null => {
+  let token = null
+  if (req && req.cookies) {
+    token = req.cookies.token
+  }
+  return token
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: cookieExtractor,
+      secretOrKey: process.env.JWT_SECRET,
+    })
+  }
+
+  validate(payload): Promise<User> {
+    return this.authService.validate(payload)
+  }
+}
